4b9ff157c2736b59c762638cf01b10158b98d2a8
Facundo La Rocca <facundo_larocca@yahoo.com.ar>
2016-10-21 12:24:28 -0300

    Vistas iniciales para Instructores

---
+++ Controllers/InstructorController.cs
@@ -0,0 +1,28 @@
+﻿using Capacitarte.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capacitarte.Controllers
+{
+    public class InstructorController : Controller
+    {
+        // GET: Instructor
+        public ActionResult Index()
+        {
+            var instructors = new List<Instructor>();
+            instructors.Add(new Instructor() { Id = 1, Name = "Facundo La Rocca" });
+            instructors.Add(new Instructor() { Id = 2, Name = "Ezequiel Fridman" });
+            instructors.Add(new Instructor() { Id = 3, Name = "Eduardo Pereyra" });
+            instructors.Add(new Instructor() { Id = 4, Name = "Christian Pereyra" });
+            instructors.Add(new Instructor() { Id = 5, Name = "Leonardo Esteves" });
+            instructors.Add(new Instructor() { Id = 6, Name = "Tomas Lopetegui" });
+
+            ViewBag.Instructors = instructors;
+
+            return View();
+        }
+    }
+}
---
+++ Models/Instructor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capacitarte.Models
+{
+    public class Instructor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
