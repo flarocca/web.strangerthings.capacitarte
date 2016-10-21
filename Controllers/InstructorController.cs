0cecaf355354730206af79762d858fe308ad71de
Facundo La Rocca <facundo_larocca@yahoo.com.ar>
2016-10-21 18:29:28 -0300

    .

--- Controllers/InstructorController.cs
+++ Controllers/InstructorController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,20 +10,45 @@ namespace Capacitarte.Controllers
 {
     public class InstructorController : Controller
     {
+        private List<Instructor> _instructors;
+
         // GET: Instructor
         public ActionResult Index()
         {
-            var instructors = new List<Instructor>();
-            instructors.Add(new Instructor() { Id = 1, Name = "Facundo La Rocca" });
-            instructors.Add(new Instructor() { Id = 2, Name = "Ezequiel Fridman" });
-            instructors.Add(new Instructor() { Id = 3, Name = "Eduardo Pereyra" });
-            instructors.Add(new Instructor() { Id = 4, Name = "Christian Pereyra" });
-            instructors.Add(new Instructor() { Id = 5, Name = "Leonardo Esteves" });
-            instructors.Add(new Instructor() { Id = 6, Name = "Tomas Lopetegui" });
-
-            ViewBag.Instructors = instructors;
+            ViewBag.Instructors = GetInstructors();
 
             return View();
+        }
+
+        // GET: Instructor
+        public ActionResult ModifyInstructor(int idInstructor)
+        {
+            ViewBag.Instructor = GetInstructors().Find((instructor) => instructor.Id == idInstructor);
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Modify(ModifyInstructorViewModel model, string returnUrl)
+        {
+            return View();
+        }
+
+        private List<Instructor> GetInstructors()
+        {
+            if (_instructors == null)
+            {
+                _instructors = new List<Instructor>();
+                _instructors.Add(new Instructor() { Id = 1, Name = "Facundo La Rocca" });
+                _instructors.Add(new Instructor() { Id = 2, Name = "Ezequiel Fridman" });
+                _instructors.Add(new Instructor() { Id = 3, Name = "Eduardo Pereyra" });
+                _instructors.Add(new Instructor() { Id = 4, Name = "Christian Pereyra" });
+                _instructors.Add(new Instructor() { Id = 5, Name = "Leonardo Esteves" });
+                _instructors.Add(new Instructor() { Id = 6, Name = "Tomas Lopetegui" });
+            }
+
+            return _instructors;
         }
     }
 }
---
+++ Models/InstructorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Capacitarte.Models
+{
+    public class ModifyInstructorViewModel
+    {
+        [Required]
+        [Display(Name = "Nombre")]
+        public string Name { get; set; }
+    }
+}
