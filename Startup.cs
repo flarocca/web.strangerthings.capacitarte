b2023084515e067603a68d256654dc2c9c0a7e52
Facundo La Rocca <facundo_larocca@yahoo.com.ar>
2016-10-21 10:28:12 -0300

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Capacitarte.Startup))]
+namespace Capacitarte
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
